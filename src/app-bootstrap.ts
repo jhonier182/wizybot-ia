82017d2d80af8a558b2581ff006342f809803503
jhonier182 <jhonier18249@gmail.com>
2026-02-10 13:25:37 -0500

    Update API base URL in index.html and add new CORS origin for deployment in app-bootstrap.ts

--- src/app-bootstrap.ts
+++ src/app-bootstrap.ts
@@ -12,6 +12,7 @@ export async function createApp(): Promise<INestApplication> {
     'http://localhost:3000',
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+    'https://wizybot-ia-iibp.vercel.app',
     /\.vercel\.app$/,
   ];
   app.enableCors({
