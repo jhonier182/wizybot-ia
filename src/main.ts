820de1b462aea2a5478c377343a39a9a2c7a0034
jhonier182 <jhonier18249@gmail.com>
2026-02-10 08:03:26 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+
+  //configuracion swagger basic
+  const config = new DocumentBuilder()
+    .setTitle('Wizybot IA')
+    .setDescription('API for the chatbot Wizybot IA')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
